78af5e97f135cc7d1d0797d330ae332651eceda4
i-am-alien <57255647+i-am-alien@users.noreply.github.com>
2023-09-25 13:00:58 -0500

    Update Peipeline.py

--- Peipeline.py
+++ Peipeline.py
@@ -4,3 +4,20 @@ vardataframe = read_csv(url_data, names=varnames)
 vararray = vardataframe.values
 varX = vararray[:,0:8]
 varY = vararray[:,8]
+
+
+
+urlfeatures = []
+urlfeatures.append(('pca', PCA(n_components=3)))
+urlfeatures.append(('select_best', SelectKBest(k=6)))
+feature_union = FeatureUnion(urlfeatures)
+# Here, pipeline is created
+estimators = []
+estimators.append(('feature_union', feature_union))
+estimators.append(('logistic', LogisticRegression()))
+model = Pipeline(estimators)
+# The pipelie is tested here
+seed = 7
+varkfold = KFold(n_splits=10)
+dataresults = cross_val_score(model, varX, varY, cv=varkfold)
+print(dataresults.mean())
